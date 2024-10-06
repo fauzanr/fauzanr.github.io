76161eff9f518c62b2d3385d5d9902aa26ceace0
root <fauzanr17@gmail.com>
2024-10-06 23:21:14 +0700

    admin chat profile pic

--- components/MainContent/ChannelRoom/ChatRoom/Chat/index.tsx
+++ components/MainContent/ChannelRoom/ChatRoom/Chat/index.tsx
@@ -6,18 +6,27 @@ import React from "react";
 const Chat = ({ chat, textOnly }: { chat: ChatType; textOnly?: boolean }) => {
   const { username, timestamp, message, isAdmin } = chat;
   const date = timestamp ? format(timestamp, "MM/dd/yyyy hh:mm a") : "";
+  const time = timestamp ? format(timestamp, "hh:mm a") : "";
   return (
     <div
       className={cn(
-        "flex w-full gap-3 px-3 py-1 mt-2 hover:backdrop-brightness-90",
+        "group flex w-full gap-3 px-3 py-1 mt-2 hover:backdrop-brightness-90",
         { "mt-0": textOnly }
       )}
     >
-      <div
-        className={cn("flex-none rounded-full w-9 h-9 bg-indigo-500", {
-          "h-0": textOnly,
-        })}
-      ></div>
+      {!textOnly ? (
+        <div
+          className={cn("flex-none rounded-full w-9 h-9 bg-indigo-500", {
+            "bg-profile-pic bg-cover bg-center": chat.isAdmin,
+          })}
+        ></div>
+      ) : (
+        <div className="flex-none w-9 relative">
+          <span className="hidden group-hover:block absolute top-1 text-[9px] text-wh2 whitespace-nowrap">
+            {time}
+          </span>
+        </div>
+      )}
       <div className="flex flex-col flex-auto">
         {!textOnly && (
           <div className="flex items-baseline gap-1">
