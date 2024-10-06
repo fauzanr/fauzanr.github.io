20da21e520a1200288eb7d0e07eebc7d1e07901d
root <fauzanr17@gmail.com>
2024-10-06 22:55:49 +0700

    simplified chat message render

--- components/MainContent/ChannelRoom/ChatRoom/Chat/index.tsx
+++ components/MainContent/ChannelRoom/ChatRoom/Chat/index.tsx
@@ -3,26 +3,37 @@ import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import React from "react";
 
-const Chat = ({ chat }: { chat: ChatType }) => {
+const Chat = ({ chat, textOnly }: { chat: ChatType; textOnly?: boolean }) => {
   const { username, timestamp, message, isAdmin } = chat;
   const date = timestamp ? format(timestamp, "MM/dd/yyyy hh:mm a") : "";
   return (
-    <div className="flex w-full gap-3 px-3 py-1 mb-2 hover:backdrop-brightness-90">
-      <div className="flex-none rounded-full w-9 h-9 bg-indigo-500"></div>
+    <div
+      className={cn(
+        "flex w-full gap-3 px-3 py-1 mt-2 hover:backdrop-brightness-90",
+        { "mt-0": textOnly }
+      )}
+    >
+      <div
+        className={cn("flex-none rounded-full w-9 h-9 bg-indigo-500", {
+          "h-0": textOnly,
+        })}
+      ></div>
       <div className="flex flex-col flex-auto">
-        <div className="flex items-baseline gap-1">
-          <span
-            className={cn("text-sm", {
-              "text-emerald-400": isAdmin,
-              "text-white": !isAdmin,
-            })}
-          >
-            {username}
-          </span>
-          <span className="text-xs text-wh2" suppressHydrationWarning>
-            {date}
-          </span>
-        </div>
+        {!textOnly && (
+          <div className="flex items-baseline gap-1">
+            <span
+              className={cn("text-sm", {
+                "text-emerald-400": isAdmin,
+                "text-white": !isAdmin,
+              })}
+            >
+              {username}
+            </span>
+            <span className="text-xs text-wh2" suppressHydrationWarning>
+              {date}
+            </span>
+          </div>
+        )}
         <div className="text-wh1 text-sm">{message}</div>
       </div>
     </div>
--- components/MainContent/ChannelRoom/ChatRoom/index.tsx
+++ components/MainContent/ChannelRoom/ChatRoom/index.tsx
@@ -3,6 +3,8 @@ import Chat from "./Chat";
 import { cn } from "@/lib/utils";
 import { useChannelStore } from "@/data/store";
 
+const SIMPLIFIED_CHAT_DELAY = 5 * 1000;
+
 const Welcome = ({ channel }: { channel: ReactNode }) => (
   <div className="flex flex-col gap-2 px-3 my-10">
     <div className="rounded-full h-16 w-16 bg-gray-700 flex items-center justify-center">
@@ -17,6 +19,7 @@ const ChatRoom = () => {
   const { id, name, chats } = useChannelStore((state) => state.channel) || {};
   const scrollRef = useRef<HTMLDivElement>(null);
   const [showContent, setShowContent] = useState(false);
+  const chatRef = useRef({ user: "", time: 0 });
 
   useEffect(() => {
     scrollRef.current?.scrollTo({ top: scrollRef.current?.scrollHeight });
@@ -27,16 +30,24 @@ const ChatRoom = () => {
     <div className="bg-bg3 h-full">
       <div
         ref={scrollRef}
-        className={cn("h-full flex flex-col overflow-y-auto", {
+        className={cn("h-full flex flex-col overflow-y-auto pb-2", {
           invisible: !showContent,
         })}
       >
         <span className="mt-auto"></span>
         <Welcome channel={name} />
         <hr className="border-gray-700 mx-2 mb-5" />
-        {chats?.map((chat) => (
-          <Chat key={chat.id} chat={chat} />
-        ))}
+        {chats?.map((chat, idx) => {
+          const time = new Date(chat.timestamp).getTime();
+          const textOnly =
+            idx !== 0 &&
+            chatRef.current.user === chat.username &&
+            time - chatRef.current.time < SIMPLIFIED_CHAT_DELAY;
+          chatRef.current.time = time;
+          chatRef.current.user = chat.username;
+
+          return <Chat key={chat.id} chat={chat} textOnly={textOnly} />;
+        })}
       </div>
     </div>
   );
