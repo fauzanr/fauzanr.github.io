5b01d6dd680b4190bd66af3ccaf8e4b665130624
root <fauzanr17@gmail.com>
2024-10-06 19:57:59 +0700

    remove asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = process.env.BASE_PATH
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: basePath + '/',
-  basePath: basePath,
 };
 
 export default nextConfig;
