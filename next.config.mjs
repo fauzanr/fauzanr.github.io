b10d488089b2e5a7227c6d4b9c57dda28cf92795
root <fauzanr17@gmail.com>
2024-10-06 15:53:58 +0700

    next config gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const repo = process.env.REPO_NAME
+
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  assetPrefix: `/${repo}/`,
+  basePath: `/${repo}`,
 };
 
 export default nextConfig;
